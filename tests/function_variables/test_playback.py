fd05ac39269c88634321796a6f26129c7d08354e
bobsnooker <luca.osullivan@education.nsw.gov.au>
2025-03-19 23:23:00 +0000

    i  love coding

--- tests/function_variables/test_playback.py
+++ tests/function_variables/test_playback.py
@@ -3,8 +3,9 @@ from function_variables.playback import playback
 string_input = "This is Killarney Heights!"
 string_output = "This...is...Killarney...Heights!"
 
+
 def test_indoor(monkeypatch, capsys):
-    monkeypatch.setattr('builtins.input', lambda: string_input)
+    monkeypatch.setattr("builtins.input", lambda: string_input)
     playback()
     captured = capsys.readouterr()
-    assert captured.out.strip()  == string_output
+    assert captured.out.strip() == string_output
