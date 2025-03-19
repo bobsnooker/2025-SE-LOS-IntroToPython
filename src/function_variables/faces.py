d136ca4b3f9889e577f37075350b5a99235fb35f
bobsnooker <luca.osullivan@education.nsw.gov.au>
2025-03-19 23:54:32 +0000

    i did faces finally

---
+++ src/function_variables/faces.py
@@ -0,0 +1,15 @@
+# :( = 🙁
+# :) = 🙂
+def main():
+    msg = input()
+    result = convert(msg)
+    print(result)
+
+
+def convert(msg):
+    msg1 = msg.replace(":)", "🙂")
+    msg2 = msg1.replace(":(", "🙁")
+    return msg2
+
+
+main()
